875b96cde1043261f4c948e75b764b772db97c7b
004Pratiksha <pratikshamakde13@gmail.com>
2024-10-01 12:42:44 +0530

    first commit

---
+++ projectJS/BMICal/script.js
@@ -0,0 +1,25 @@
+function calculateBMI() {
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value / 100; // Convert cm to meters
+
+    if (weight > 0 && height > 0) {
+        const bmi = (weight / (height * height)).toFixed(2);
+
+        let category = '';
+        if (bmi < 18.5) {
+            category = 'Underweight';
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            category = 'Normal weight';
+        } else if (bmi >= 25 && bmi <= 29.9) {
+            category = 'Overweight';
+        } else {
+            category = 'Obesity';
+        }
+
+        document.getElementById('bmi').textContent = bmi;
+        document.getElementById('category').textContent = category;
+        document.getElementById('result').classList.remove('hidden');
+    } else {
+        alert('Please enter valid weight and height.');
+    }
+}
